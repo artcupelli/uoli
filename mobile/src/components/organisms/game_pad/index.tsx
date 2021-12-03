2b2cf7bf238663a58f67a557a173b44efa2a5474
Artur Cupelli <artur.cupelli@usp.br>
2021-12-03 01:16:47 -0300

    Implementa gamepad

--- mobile/src/components/organisms/game_pad/index.tsx
+++ mobile/src/components/organisms/game_pad/index.tsx
@@ -6,10 +6,48 @@ import Draggable from 'react-native-draggable';
 
 import { Colors } from '../../../constants/colors';
 
+import { CarControlService } from '../../../service/car_control_service';
+
 import { styles } from './game_pad_style';
 
 
+
 const GamePad: React.FC = () => {
+
+  const carController = new CarControlService();
+
+  async function handlePress(event: any): Promise<void> {
+    const X = event.nativeEvent.locationX;
+    const Y = event.nativeEvent.locationY;
+
+
+    console.log(Y + "     " + X)
+
+    if (Y >= 200) {
+      console.log("FRENTE")
+      await carController.goFoward();
+
+    } else if (Y <= 30) {
+      console.log("TRAS")
+      await carController.goBack();
+    }
+
+    if (X >= 780) {
+      console.log("DIREITA")
+      await carController.goRight();
+
+    } else if (X <= 6) {
+      console.log("ESQUERDA")
+      await carController.goLeft();
+    }
+
+  }
+
+  async function stop(): Promise<void> {
+    console.log("STOP")
+    await carController.stop();
+  }
+
 
   return (
     <View style={styles.container}>
@@ -23,6 +61,8 @@ const GamePad: React.FC = () => {
         shouldReverse
         renderSize={80}
         renderText=""
+        onDrag={handlePress}
+        onDragRelease={stop}
       />
     </View>
   );
